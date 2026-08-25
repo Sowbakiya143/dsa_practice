5525d9ccb1810e41dc73e3e55538a0878fa7ed10
sowbakiya <sowbakiya1403@gmail.com>
2026-08-25 21:37:23 +0530

    1

---
+++ print_byindex.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class print_byindex{
+  public static void main(String args[]){
+    int[] arr={10,20,30,40,50};
+    for(int i=0;i<arr.length;i++){
+    System.out.println(i+"->"+arr[i]);
+    }
+  }
+}
