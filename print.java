d92e99989c633fcb1e771e9533972aeee2200bef
sowbakiya <sowbakiya1403@gmail.com>
2026-08-25 21:44:43 +0530

    2

---
+++ print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class print{
+  public static void main(String args[]){
+    int[] arr={10,20,30,40,50};
+    System.out.println("first"+" "+arr[0]);
+    System.out.println("last"+" "+arr[4]);
+  }
+}
